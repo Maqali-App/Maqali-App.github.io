70b3c2ce2e36d484ef071fceedf8824a36f872dc
yusuf1164 <130356952+yusuf1164@users.noreply.github.com>
2026-08-22 07:58:51 +0100

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,18 @@
+// Firebase Configuration Setup
+const firebaseConfig = {
+  apiKey: "AIzaSyAs1A-I-TgTLPxthSxa0D4e-R6pmsk70FU",
+  authDomain: "maqali-app-83b95.firebaseapp.com",
+  databaseURL: "https://maqali-app-83b95-default-rtdb.firebaseio.com",
+  projectId: "maqali-app-83b95",
+  storageBucket: "maqali-app-83b95.firebasestorage.app",
+  messagingSenderId: "458036803827",
+  appId: "1:458036803827:web:a7d2f61d1256fdfca21f86",
+  measurementId: "G-RE6DXYQ0RL"
+};
+
+// Initialize Firebase App instance if not already initialized
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const auth = firebase.auth();
