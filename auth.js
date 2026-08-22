02cad5364a0b67cee237956b87b8c5253c6399ac
yusuf1164 <130356952+yusuf1164@users.noreply.github.com>
2026-08-22 09:01:53 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,33 +1,16 @@
-// Initialize Firebase App
-const firebaseConfig = {
-  apiKey: "AIzaSyAs1A-I-TgTLPxthSxa0D4e-R6pmsk70FU",
-  authDomain: "maqali-app-83b95.firebaseapp.com",
-  databaseURL: "https://maqali-app-83b95-default-rtdb.firebaseio.com",
-  projectId: "maqali-app-83b95",
-  storageBucket: "maqali-app-83b95.firebasestorage.app",
-  messagingSenderId: "458036803827",
-  appId: "1:458036803827:web:a7d2f61d1256fdfca21f86",
-  measurementId: "G-RE6DXYQ0RL"
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-const auth = firebase.auth();
-
 // Centralized Navigation Guard
 auth.onAuthStateChanged((user) => {
-  const currentPage = window.location.pathname.split("/").pop();
+  const path = window.location.pathname;
+  const isLoginPage = path.endsWith("login.html");
 
   if (user) {
-    // If logged in and sitting on login page, send to index
-    if (currentPage === "login.html" || currentPage === "") {
+    // If logged in and on the login page, go to main dashboard
+    if (isLoginPage) {
       window.location.href = "index.html";
     }
   } else {
-    // If logged out and trying to access main dashboard, send to login
-    if (currentPage !== "login.html") {
+    // If logged out and NOT on the login page, go to login page
+    if (!isLoginPage) {
       window.location.href = "login.html";
     }
   }
