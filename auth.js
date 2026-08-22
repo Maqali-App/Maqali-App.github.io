d90ed7204b10e077e7a2dd447e92b8bdfcb785ff
yusuf1164 <130356952+yusuf1164@users.noreply.github.com>
2026-08-22 08:16:29 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,18 +1,16 @@
-// Firebase Configuration Setup
-const firebaseConfig = {
-  apiKey: "AIzaSyAs1A-I-TgTLPxthSxa0D4e-R6pmsk70FU",
-  authDomain: "maqali-app-83b95.firebaseapp.com",
-  databaseURL: "https://maqali-app-83b95-default-rtdb.firebaseio.com",
-  projectId: "maqali-app-83b95",
-  storageBucket: "maqali-app-83b95.firebasestorage.app",
-  messagingSenderId: "458036803827",
-  appId: "1:458036803827:web:a7d2f61d1256fdfca21f86",
-  measurementId: "G-RE6DXYQ0RL"
-};
+// auth.js
+firebase.auth().onAuthStateChanged((user) => {
+  const currentPage = window.location.pathname.split("/").pop();
 
-// Initialize Firebase App instance if not already initialized
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-const auth = firebase.auth();
+  if (user) {
+    // User is logged in
+    if (currentPage === "login.html" || currentPage === "") {
+      window.location.href = "index.html";
+    }
+  } else {
+    // User is logged out
+    if (currentPage !== "login.html") {
+      window.location.href = "login.html";
+    }
+  }
+});
